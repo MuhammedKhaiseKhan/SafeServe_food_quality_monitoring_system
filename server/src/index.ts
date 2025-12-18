bdd8067b494b6bfd9c3877634d0588868aefa3ae
MuhammedKhaiseKhan <khzkhanzz@gmail.com>
2025-12-19 01:15:52 +0530

    cors refix

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,11 +12,8 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "*",
-    credentials: true
-}));
-app.use(helmet());
+app.use(cors());
+// app.use(helmet());
 app.use(morgan('dev'));
 
 import authRoutes from './routes/auth.routes';
